9596265c72338d8341318950f200c988cad3f708
Yukaichen <YuKaichen@micb2b.com>
2026-02-27 15:58:40 +0800

    feat(app): 整合 CookieConsent 元件至 App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import UseCases from './components/UseCases';
 import Pricing from './components/Pricing';
 import CallToAction from './components/CallToAction';
 import Footer from './components/Footer';
+import CookieConsent from './components/CookieConsent';
 
 function App() {
     return (
@@ -20,6 +21,7 @@ function App() {
                 <CallToAction />
             </main>
             <Footer />
+            <CookieConsent />
         </div>
     );
 }
